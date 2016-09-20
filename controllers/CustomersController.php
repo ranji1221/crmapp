77d39927f187dcc61f3b68be8ed673697dd067c1
ranji1221 <jiran1221@163.com>
2016-09-20 17:01:30 +0800

    1. 编辑CustomersController.php对象

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -9,19 +9,53 @@
 	class CustomersController extends Controller{
 		
 		public function actionIndex(){
-			$this->printCustomer(2);
+			//-- 1. 顾客数据
+			$customer = new Customer('必然', strtotime('2016-09-10'));      //-- strtotime()返回的应该是个int
+			$customer->notes = '这是个完人，没毛病';
+			//-- 2. 顾客电话数据(两种写法)
+			/* 
+			 *第一种写法：那么 store()方法里的循环部分的代码就是：$phone_record->number = $phone->number; (属性)
+			$p1 = new Phone();
+			$p1->number = '170';
+			$p2 = new Phone();
+			$p2->number = '171';
+			$p3 = new Phone();
+			$p3->number = '172';
+			$customer->phones = [$p1, $p2, $p3];
+			*/
+			//第二种写法：那么store()方法里的循环部分的代码就是：$phone_record->number = $phone['number'];  (数组下标的形式)
+			$customer->phones = [
+					['number'=>'188'],
+					['number'=>'189'],
+					['number'=>'190']
+			];
+			
+			
+			//-- 3. 存储顾客信息
+			$this->store($customer);
+			//-- 4. 打印信息
+			$this->printCustomer(6);
 		}
-		
+		/**
+		 * 根据顾客信息存储顾客记录
+		 * @param Customer $customer
+		 */
 		private function store(Customer $customer){
+			//-- 1. 创建CustomerRecord对象
 			$customer_record = new CustomerRecord();
+			//-- 2. 给CustomerRecord对象赋值，这里唯一要注意的是日期类型的赋值
+			//-- 切记我们拿到的customer的值是个整型，必须要转为日期
 			$customer_record->name = $customer->name;
-			$customer_record->birth_date = $customer->birth_date->format('Y-m-d');
+			$customer_record->birth_date =date('Y-m-d',$customer->birth_date);
 			$customer_record->notes = $customer->notes;
 			
+			//-- 3. 存储CustomerRecord，并且存储相关的电话信息
 			if($customer_record->save())
 				foreach ($customer->phones as $phone){
 						$phone_record = new PhoneRecord();
-						$phone_record->number = $phone->number;
+						//-- 注意这里的写法，如果你在构造customer->phones对象数组的时候，里面是new的phone对象，那么就
+						//-- 写成$phone->number; 如果里面都是用的数组，那么就用现在的下标的访问方式$phone['number']
+						$phone_record->number = $phone['number'];		
 						$phone_record->customer_id =$customer_record->id;
 						$phone_record->save();
 				}
