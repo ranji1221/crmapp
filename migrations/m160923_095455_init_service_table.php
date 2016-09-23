0cd80279ed198c03df7ba84e2e00befd29fe896f
ranji1221 <jiran1221@163.com>
2016-09-23 18:31:39 +0800

    安装yii2框架中的gii组件
    1. 在migrations目录下增加了_init_service_table.php的建库文件
    2. 主要是安装yii2中的gii组件，这里只谈需要注意的地方：
       a. 目前为止我们安装了4个框架类的东东，codecept、faker、yii2、gii
       b. 安装的命令格式基本是composer require "codeceptoin/codeception:*"
       c. 但是由于安装yii2、gii的时候，yii2这个框架是基于asset这个插件的，所以先要安装asset插件。
       d. 即为先执行：composer global require "fxp/composer-asset-plugin:~1.1.1"
    (这里需要注意版本)命令
       e. 再执行: composer require --prefer-dist "yiisoft/yii2:*" 或者  composer
    require --prefer-dist "yiisoft/yii2-gii:*"

---
+++ migrations/m160923_095455_init_service_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+class m160923_095455_init_service_table extends Migration
+{
+    public function up()
+    {
+		$tableOptions = '';
+		if($this->db->driverName === 'mysql')
+			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+		$this->createTable('service', [
+				'id' => $this->primaryKey(),
+				'name' => 'string unique',				//-- 服务项目的名称
+				'hourly_rate' => 'integer',				//-- 服务项目的费率（按小时计费）
+		]);
+    }
+
+    public function down()
+    {
+       $this->dropTable('service');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
