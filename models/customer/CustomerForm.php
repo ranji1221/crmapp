8ddf00ffbcf0b5341938287d761439ee0bc763f2
ranji1221 <jiran1221@163.com>
2016-09-26 09:17:30 +0800

    解决了model类里的日期类型校验通不过的问题
    1.该问题很隐蔽，就是format=>'Y-m-d'，然后页面些的格式没问题就是通不过。
    2.一定要加载php中的extension=php_intl.dll选项
    3.该问题的解释我在models\customer\Customer.php、CustomerForm.php里的rules方法里有详细的解释说明，大家可以查看。

--- models/customer/CustomerForm.php
+++ models/customer/CustomerForm.php
@@ -17,7 +17,18 @@
 		public function  rules(){
 			return [
 					 [['name', 'birth_date','notes'], 'default', 'value' => null],
-					 ['birth_date','date','format'=>'Y-m-d'],
+					 /**
+					  * 这个时期类型，差点把人弄死。原来这个格式是有问题的，返回来看这个问题其实就是
+					  * 先访问项目的crmapp/web/requirements.php文件夹，把里面的错误和Warning基本都要修复了。
+					  * 否则，我这里的这个问题不是兰大神，就太难处理了。
+					  * 其实，就是格式的问题, 如果写成“format=>Y-m-d”，其实用的是php的格式，那么yii2框架要求必须
+					  * 加入扩展extension=php_intl.dll的国际化扩展，否则验证是通不过的。
+					  * 如果不加入这个扩展，非要使用这种格式的话就写成：“format=>php:Y-m-d”即可。
+					  * 如果上面两种都不想使用的话，那就用国际标准的格式，网上和书里给的例子都是不靠谱的。 
+					  * 国际标准的格式在yii\helpers\BaseFormatConverter.php里的有提示信息
+					  * 例如：'format'=>'yyyy-MM-dd'
+					  */
+					 ['birth_date','date','format'=>'php:Y-m-d'],
 			];
 		}
 	}
